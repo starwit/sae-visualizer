061713082917f9d5296a3026b988faf165e4e4f5
ztarbug <markus.zarbock@starwit.de>
2025-03-17 15:15:13 +0100

    work on security

--- application/src/main/java/de/starwit/SaeVisualizerApplication.java
+++ application/src/main/java/de/starwit/SaeVisualizerApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication(exclude = {
-	org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class })
+	org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+	org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class })
 public class SaeVisualizerApplication {
 
 	public static void main(String[] args) {
